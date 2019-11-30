e5a37715317213c0e9578ea933e297f13d8fff2a
openanswer <mathemagicalhelper@gmail.com>
2019-11-30 03:09:49 -0500

    Minor bugfixes - looks like the end result of the tutorial is not the same as the tutorial itself

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -5,37 +5,33 @@
 // To use `connect()` define a special `mapStateToProps` that describes
 //   how to transform the current Redux store state into the props we need to pass to presentational component we are wrapping.
 
+import {connect} from 'react-redux';
 import {toggleTodo} from "../actions";
 import TodoList from '../components/TodoList';
-import {connect} from 'react-redux';
+import {VisibilityFilters} from "../actions";
 
 const getVisibleTodos = (todos, filter) => {
     switch (filter) {
-        case 'SHOW_COMPLETED':
-            return todos.filter(t=>t.completed);
-        case 'SHOW_ACTIVE':
+        case VisibilityFilters.SHOW_ALL:
+            return todos
+        case VisibilityFilters.SHOW_ACTIVE:
             return todos.filter(t=>!t.completed);
-        case 'SHOW_ALL':
+        case VisibilityFilters.SHOW_COMPLETED:
+            return todos.filter(t=>t.completed);
         default:
-            return todos;
+            throw new Error('Unknown filter: ' + filter)
     }
 };
 
 // VisibleTodoList's mapStateToProps
-const mapStateToProps = state => {
-    return {
-        todos: getVisibleTodos(state.todos, state.visibilityFilter)
-    }
-};
+const mapStateToProps = state => ({
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+});
 
 // Container components can also dispatch actions (to tell redux to update the state)
-const mapDispatchToProps = dispatch => {
-    return {
-        onTodoClick: id => {
-            dispatch(toggleTodo(id))
-        }
-    }
-};
+const mapDispatchToProps = dispatch => ({
+    toggleTodo: id => {dispatch(toggleTodo(id))}
+});
 
 const VisibleTodoList = connect(
     mapStateToProps,
